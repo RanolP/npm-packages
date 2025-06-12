4d7aa2a6fd25597db62acfa593bbf684f91c87de
RanolP <ranolp@laftel.net>
2025-06-12 14:05:14 +0900

    add raw field for each segment

--- packages/soonloh/src/index.ts
+++ packages/soonloh/src/index.ts
@@ -3,4 +3,5 @@ export * from './core/config.js';
 export * from './core/parser/classifier.js';
 export * from './core/parser/segmenter.js';
 export * from './core/parser/parser.js';
+export * from './core/types.js';
 export * from './builtin-common-segment.js';
