2beb78ebc369ab127b52a5a3a430a2e9c283fe87
RanolP <me@ranolp.dev>
2025-06-12 13:27:16 +0900

    [soonloh] expose CommonSegment (#14)
    * expose common segment
    
    * add changeset

---
+++ .changeset/full-cups-cheer.md
@@ -0,0 +1,5 @@
+---
+"soonloh": patch
+---
+
+Expose CommonSegment
--- packages/soonloh/src/index.ts
+++ packages/soonloh/src/index.ts
@@ -3,3 +3,4 @@ export * from './core/config.js';
 export * from './core/parser/classifier.js';
 export * from './core/parser/segmenter.js';
 export * from './core/parser/parser.js';
+export * from './builtin-common-segment.js';
